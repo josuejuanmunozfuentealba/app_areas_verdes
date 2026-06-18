649527150375e81f89b859c4629c49833ed25979
josuejuanmunozfuentealba <josuejuanmunozfuentealba@users.noreply.github.com>
2026-06-18 20:22:16 +0000

    deploy: bd68e7284bad289d80b125bb55b3183634a85e00

--- main.dart.js
+++ main.dart.js
@@ -34041,7 +34041,7 @@ $S:2}
 B.aHf.prototype={
 $1(a){var s=B.dR().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/c416acfeb8126e097f758c664aaa3da929e27da0/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
 $S:56}
 B.SO.prototype={
 gn(a){var s=this.a
