ebf0ecdff65849c8c645e2d72528f75dee8c1fae
josuejuanmunozfuentealba <j.munoz373@alumnos.santotomas.cl>
2026-06-22 17:06:13 +0000

    Deploying to gh-pages from @ josuejuanmunozfuentealba/app_areas_verdes@e2978e3c206a3b8b0ba3f5a71972313799698765 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3540822497" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4236368302" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
