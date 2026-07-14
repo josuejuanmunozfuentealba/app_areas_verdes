ef908a0e70ce34df6b45188664d74f8100001a95
Josue Juan Muñoz Fuentelba <j.munoz373@alumnos.santotomas.cl>
2026-07-14 09:47:48 -0400

    Update pdf_export_service.dart

--- lib/services/pdf_export_service.dart
+++ lib/services/pdf_export_service.dart
@@ -80,7 +80,7 @@ class PDFExportService {
     // Cargar el logo desde assets
     pw.ImageProvider? logoImage;
     try {
-      final imageData = await rootBundle.load('assets/assets/logo_2026.png');
+      final imageData = await rootBundle.load('assets/logo_2026.png');
       final bytes = imageData.buffer.asUint8List();
       logoImage = pw.MemoryImage(bytes);
     } catch (e) {
